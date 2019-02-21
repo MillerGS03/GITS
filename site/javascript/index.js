ba9dbfa67cc857297855fc1757ee7cb5fbeb5433
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-02-21 10:26:03 -0300

    Update index.js

--- site/javascript/index.js
+++ site/javascript/index.js
@@ -7,10 +7,10 @@ if (user)
     var body = document.getElementsByTagName('body')[0];
     var cod = `
     
-      <ul id="slide-out" class="sidenav">
+      <ul id="slide-out-batata" class="sidenav" id="triggerEsquerda">
         <img src="https://static1.conquistesuavida.com.br/ingredients/5/54/52/05/@/24682--ingredient_detail_ingredient-2.png">
       </ul>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Crunchyroll_logo_2012v.png" data-target="slide-out" class="sidenav-trigger">`;
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Crunchyroll_logo_2012v.png" data-target="slide-out-batata" class="sidenav-trigger">`;
 
 
     body.innerHTML += cod;
