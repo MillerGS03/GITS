c3940fe9d94503cee31d0b5873c1ff177bb41d0e
Scherer <fsvicentin@hotmail.com>
2019-02-21 09:48:46 -0300

    imagem abre o menu lateral

---
+++ site/javascript/index.js
@@ -0,0 +1,21 @@
+var user = new Object();
+
+
+
+if (user)
+{
+    var body = document.getElementsByTagName('body')[0];
+    var cod = `
+    
+      <ul id="slide-out" class="sidenav">
+        <img src="https://static1.conquistesuavida.com.br/ingredients/5/54/52/05/@/24682--ingredient_detail_ingredient-2.png">
+      </ul>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Crunchyroll_logo_2012v.png" data-target="slide-out" class="sidenav-trigger">`;
+
+
+    body.innerHTML += cod;
+}
+else
+{
+    console.log(';-;')
+}
