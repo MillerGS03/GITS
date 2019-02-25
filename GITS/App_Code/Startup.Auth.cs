1cb556d2b7e19c8cf31d3ae4b092a9bb3cf46ec9
Scherer <fsvicentin@hotmail.com>
2019-02-25 12:00:00 -0300

    união do asp.net
    aeeee

---
+++ GITS/App_Code/Startup.Auth.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+namespace GITS
+{
+    public partial class Startup {
+
+        // Para obter mais informações sobre autenticação de configuração, visite https://go.microsoft.com/fwlink/?LinkId=301883
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Permitir que o aplicativo use um cookie para armazenar informações para o usuário conectado
+            // e também armazenar informações sobre um usuário que faz logon com um provedor de logon de terceiros.
+            // Isso é obrigatório se o aplicativo permitir que usuários façam logon
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Remover comentário das linhas a seguir para permitir logon com provedores de logon de terceiros
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
+
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+        }
+    }
+}
---
+++ GITS/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GITS.Startup))]
+namespace GITS
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
