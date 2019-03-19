c16ba1e9580b02247fc11c6bbf2347b78229df9d
Scherer <fsvicentin@hotmail.com>
2019-03-19 11:48:25 -0300

    Esquerda

--- GITS/GITS/site/javascript/index.js
+++ GITS/GITS/site/javascript/index.js
@@ -10,18 +10,69 @@ user.Decoracao = 1;
 user.TemaSite = 1;
 user.Dinheiro = 500;
 
+var triggerEsquerda = 0;
+var estaAbrindoEsquerda = false;
+
 $(document).ready(function(){
   $('#slide-out').sidenav({
     edge: 'right'
   });
-  $('#slide-out-batata').sidenav();
+  $('#slide-out-esquerda').sidenav();
   $(".dropdown-trigger").dropdown({
     constrainWidth: false
   });
 });
 
 if (user)
+  tratar(user);
+
+function tratar(user)
 {
-  $(".imgPerfilBody").html(`<img src="${user.FotoPerfil}" data-target="slide-out-esquerda" class="sidenav-trigger">`);
-  $("#imgPerfil").prop('src', user.FotoPerfil);
+  $("#main").html(`
+    <div id="slideEsquerda">
+      <img src="" id="imgPerfil">
+    </div>
+  `);
+  $("#slideEsquerda").height(document.getElementById('footer').getBoundingClientRect().top - $(".nav-wrapper").height());
+  $("#slideEsquerda").after(`
+    <div class="hexagon" id="triggerEsquerda" style="transition: 0.75s;left: ${document.getElementById('slideEsquerda').style.left + document.getElementById('slideEsquerda').style.width - 165}px;"></div>
+  `);
+  $("#triggerEsquerda").on('click', function(e){
+    if (!estaAbrindoEsquerda)
+    {
+      $(".ripple").remove();
+      var posX = $(this).offset().left,
+          posY = $(this).offset().top,
+          buttonWidth = $(this).width(),
+          buttonHeight =  $(this).height();
+      
+      $(this).prepend("<span class='ripple'></span>");
+    
+      
+      if(buttonWidth >= buttonHeight) {
+        buttonHeight = buttonWidth;
+      } else {
+        buttonWidth = buttonHeight; 
+      }
+      buttonHeight = 50;
+      var x = e.pageX - posX - buttonWidth / 2;
+      var y = e.pageY - posY - buttonHeight / 2;
+      
+    
+      $(".ripple").css({
+        width: buttonWidth,
+        height: buttonHeight,
+        top: y + 'px',
+        left: x + 'px'
+      }).addClass("rippleEffect");
+
+      estaAbrindoEsquerda = true;
+      setTimeout(function(){estaAbrindoEsquerda = false;}, 750)
+      var left = -triggerEsquerda*$("#slideEsquerda").width() + $("#slideEsquerda").width() - 165;
+      $("#slideEsquerda").css('left', (-triggerEsquerda*$("#slideEsquerda").width()) + "px");
+      console.log(left)
+      $("#triggerEsquerda").css('left', left + "px")
+      triggerEsquerda = Math.abs(triggerEsquerda - 1);
+    }
+  })
 }
