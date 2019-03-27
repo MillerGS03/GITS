2027c4ccc1c359e6d68835f91c34432b7ae92c7f
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-03-27 11:32:06 -0300

    Página de login terminada

--- GITS/GITS/site/javascript/login.js
+++ GITS/GITS/site/javascript/login.js
@@ -1,3 +1,15 @@
-$(document).on("resize", function() {
-    $("#conteinerTabela").css("top", $("#wrapperLogin").width() + $("nav").width() + "px");
+var redimensionarLogin = function() {
+    var conteinerTabela = $("#conteinerTabela");
+    conteinerTabela.css("top", 20 + $("#wrapperLogin").height() + $("nav").height() + "px");
+    $("#footer").css("top", conteinerTabela.offset().top + conteinerTabela.height() + "px");
+    $("#loginGoogle").css("top", $("#divisaoLogin").position().top + ($("#login").height() - $("#divisaoLogin").position().top - $("#loginGoogle").height()) / 2 + "px");
+    $("#loginGoogle").css("left", ($("#login").outerWidth() - $("#loginGoogle").outerWidth()) / 2 + "px")
+}
+
+$(document).ready(function() {
+    redimensionarLogin();
+    setTimeout(redimensionarLogin, 1000);
+})
+$(window).on("resize", function() {
+    redimensionarLogin();
 })
