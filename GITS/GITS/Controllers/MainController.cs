10fb022c9a66c64af5ef152e62c3f6011edefd92
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-04-15 11:54:03 -0300

    Batata frita
    Node_modules added

---
+++ GITS/GITS/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GITS.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Login()
+        {           
+            return Request.Cookies["user"] != null ? View("Index") : View("Login");
+        }
+
+        public ActionResult Index()
+        {
+            return Request.Cookies["user"] != null ? View("Index") : View("Login");
+        }
+        public ActionResult Calendario()
+        {
+            return PartialView();
+        }
+    }
+}
