c8a4cc8a9fc43aa02b1ec4feeb6a999713faa524
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-05-14 12:11:36 -0300

    Exibição de metas

--- GITS/GITS/ViewModel/Meta.cs
+++ GITS/GITS/ViewModel/Meta.cs
@@ -18,6 +18,10 @@ namespace GITS.ViewModel
                 Data = s["Data"].ToString().Substring(0, 10);
                 Progresso = Convert.ToInt16(s["Progresso"]);
                 UltimaInteracao = s["UltimaInteracao"].ToString().Substring(0, 10);
+                Recompensa = (float)s["Recompensa"];
+                GitcoinsObtidos = (float)s["GitcoinsObtidos"];
+                TarefasCompletas = Convert.ToInt32(s["GitcoinsObtidos"]);
+                
             }
             catch { }
         }
@@ -25,7 +29,7 @@ namespace GITS.ViewModel
         {
 
         }
-        public Meta(int codMeta, string titulo, string descricao, string data, int progresso, string ultimaInteracao)
+        public Meta(int codMeta, string titulo, string descricao, string data, int progresso, string ultimaInteracao, float recompensa, float gitcoinsObtidos, int tarefasCompletas)
         {
             CodMeta = codMeta;
             Titulo = titulo;
@@ -33,11 +37,18 @@ namespace GITS.ViewModel
             Data = data;
             Progresso = progresso;
             UltimaInteracao = ultimaInteracao;
+            Recompensa = recompensa;
+            GitcoinsObtidos = gitcoinsObtidos;
+            TarefasCompletas = tarefasCompletas;
         }
 
         public int CodMeta { get; set; }
         public int Progresso { get; set; }
+        public float Recompensa { get; set; }
+        public float GitcoinsObtidos { get; set; }
+        public int TarefasCompletas { get; set; }
         public string UltimaInteracao { get; set; }
+        public List<Tarefa> TarefasRelacionadas { get => Dao.Exec($"select * from Tarefa where CodTarefa in (select CodTarefa from TarefaMeta where CodMeta = {CodMeta}", new List<Tarefa>()); }
 
         public override bool Equals(object obj)
         {
@@ -47,14 +58,21 @@ namespace GITS.ViewModel
             return meta != null &&
                    CodMeta == meta.CodMeta &&
                    Progresso == meta.Progresso &&
-                   UltimaInteracao == meta.UltimaInteracao;
+                   UltimaInteracao == meta.UltimaInteracao &&
+                   Recompensa == meta.Recompensa &&
+                   GitcoinsObtidos == meta.GitcoinsObtidos &&
+                   TarefasCompletas == meta.TarefasCompletas;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = base.GetHashCode();
+            var hashCode = 1389737090;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
             hashCode = hashCode * -1521134295 + CodMeta.GetHashCode();
             hashCode = hashCode * -1521134295 + Progresso.GetHashCode();
+            hashCode = hashCode * -1521134295 + Recompensa.GetHashCode();
+            hashCode = hashCode * -1521134295 + GitcoinsObtidos.GetHashCode();
+            hashCode = hashCode * -1521134295 + TarefasCompletas.GetHashCode();
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(UltimaInteracao);
             return hashCode;
         }
