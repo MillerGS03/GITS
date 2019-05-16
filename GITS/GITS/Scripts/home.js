17d9655b1e497618af8e30874646f33a7eeefb5f
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-05-16 11:57:26 -0300

    Modal adicionar meta

--- GITS/GITS/Scripts/home.js
+++ GITS/GITS/Scripts/home.js
@@ -812,6 +812,28 @@ function tratar(user) {
         }
     });
     mostrarXP(user);
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        onstart: () => { $('.datepicker').appendTo('body');},
+        i18n: {
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabádo'],
+            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            today: 'Hoje',
+            clear: 'Limpar',
+            close: 'Pronto',
+            labelMonthNext: 'Próximo mês',
+            labelMonthPrev: 'Mês anterior',
+            labelMonthSelect: 'Selecione um mês',
+            labelYearSelect: 'Selecione um ano',
+            selectMonths: true,
+            selectYears: 15,
+            cancel: 'Cancelar',
+            clear: 'Limpar'
+        }
+    });
     $("#triggerEsquerda").on('click', function (e) {
         if (!estaAbrindoEsquerda) {
             estaAbrindoEsquerda = true;
