22a6f776a312511e9078c1e3d7bf321ed54bb15a
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2019-05-28 11:43:56 -0300

    batata azulada implementada

--- GITS/GITS/Scripts/forum.js
+++ GITS/GITS/Scripts/forum.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     $(".tabs").tabs();
 })
 function mostrarTab(item) {
-    var itemCapitalizado = item.substring(0, 0).toUpperCase() + item.substring(1);
-
+    var itemCapitalizado = item.substring(0, 1).toUpperCase() + item.substring(1);
+    $("li.collection-item.active").removeClass("active");
+    if (!$("#item" + itemCapitalizado).hasClass("active")) {
+        $("#item" + itemCapitalizado).addClass("active");
+        window.location.href = "#" + item;
+    }
 }
